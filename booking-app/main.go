0d5cb750dc866b8f7badb066b6b210791dc80b79
Francis  Labi <francislabi@Franciss-MacBook-Pro.local>
2023-07-25 22:11:04 +0100

    playing around with %T placeholder

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,4 +10,13 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	fmt.Printf("conferenceName is a %T, conferenceTickets is a %T, remainingTickets is a %T\n", conferenceName, conferenceTickets, remainingTickets)
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
 }
