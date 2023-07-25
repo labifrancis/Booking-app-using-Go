0ffaf623ee54d3e931ce79579bc329ce5ce1b372
Francis  Labi <francislabi@Franciss-MacBook-Pro.local>
2023-07-25 23:05:59 +0100

    gtting user imput using Scan anand pointers &

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
@@ -15,7 +15,8 @@ func main() {
 	var userName string
 	var userTickets int
 	// ask user for their name
-	userName = "Tom"
+	fmt.Scan(&userName)
+
 	userTickets = 2
 	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 
